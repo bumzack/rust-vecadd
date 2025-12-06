19dd3a5ec44cd1b01565b26df01ea417a8f26330
bumzack <bumzacktrara@gmail.com>
2025-12-06 20:18:45 +0100

    well - nope

--- build.rs
+++ build.rs
@@ -10,7 +10,7 @@ fn main() {
     let out_path = path::PathBuf::from(env::var("OUT_DIR").unwrap());
     let manifest_dir = path::PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    CudaBuilder::new(manifest_dir.join("kernels"))
+    CudaBuilder::new(manifest_dir.join("kernel"))
         .copy_to(out_path.join("kernels.ptx"))
         .build()
         .unwrap();
